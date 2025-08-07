0fd9d880ef2595593e0ac6dece9a0dd85c20aba0
Agung Gunawan <agg.nst772@gmail.com>
2025-08-07 19:20:16 +0700

    Quit

--- Runtime/System/Game.cs
+++ Runtime/System/Game.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using Object = UnityEngine.Object;
 
 namespace Gamecore
@@ -92,6 +93,11 @@ namespace Gamecore
             }
 
             return null;
+        }
+
+        public static void Quit()
+        {
+            Application.Quit();
         }
     }
 }

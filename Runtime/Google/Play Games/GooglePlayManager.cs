8bbbe4df730f129c3ec67f6b08009f04f5df1fdd
Agung Gunawan <agg.nst772@gmail.com>
2025-08-12 20:40:46 +0700

    Rev GoogleLeaderboard v4

--- Runtime/Google/Play Games/GooglePlayManager.cs
+++ Runtime/Google/Play Games/GooglePlayManager.cs
@@ -9,9 +9,8 @@ namespace Gamecore
     {
         public bool IsAuthenticate {  get; private set; }
         public GoogleLeaderboard Leaderboard {  get; private set; }
-        public override void Initialize()
+        public void Authenticate()
         {
-            base.Initialize();
             Debug.Log("Loading login with Google Play games...");
             Leaderboard = new GoogleLeaderboard();
             PlayGamesPlatform.Activate();

f3e1543457679cf95cf5a494e7e016017122c4e5
Agung Gunawan <agg.nst772@gmail.com>
2025-08-14 15:57:54 +0700

    Refactor Google v6

--- Runtime/Google/Play Games/PlaygameLeaderboard.cs
+++ Runtime/Google/Play Games/PlaygameLeaderboard.cs
@@ -5,16 +5,11 @@ using UnityEngine;
 
 namespace Gamecore.Google
 {
-    public class PlaygameLeaderboard : AdBase
+    public class PlaygameLeaderboard : PlaygameBase
     {
-        public override bool CanShow()
-        {
-            return true;
-        }
-
         public void ReportScore(long skor)
         {
-            PlayGamesPlatform.Instance.ReportScore(skor, Game.Get<AdManager>().Setting.leaderboardID, success =>
+            PlayGamesPlatform.Instance.ReportScore(skor, Game.Get<PlaygameManager>().Setting.leaderboardID, success =>
             {
                 Debug.Log(success ? "Skor terkirim" : "Gagal kirim skor");
             });

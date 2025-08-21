268d286f6b38ab93089011e904f80339a8659907
Agung Gunawan <agg.nst772@gmail.com>
2025-08-21 17:16:11 +0700

    Transform Extensions

---
+++ Runtime/Utility/Transform/TransformExtensions.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gamecore
+{
+    public static class TransformExtensions
+    {
+        public static void DestroyAllChildren(this Transform parent, bool immediate = false)
+        {
+            if (parent == null) return;
+            while (parent.childCount > 0)
+            {
+                if (immediate)
+                    Object.DestroyImmediate(parent.GetChild(0).gameObject);
+                else
+                    Object.Destroy(parent.GetChild(0).gameObject);
+            }
+        }
+        
+        public static void DestroyFirstChild(this Transform parent, bool immediate = false)
+        {
+            if (parent == null) return;
+            if (parent.childCount == 0) return;
+
+            var _child = parent.GetChild(0).gameObject;
+            if (immediate)
+                Object.DestroyImmediate(_child);
+            else
+                Object.Destroy(_child);
+        }
+        
+        public static void DestroyLastChild(this Transform parent, bool immediate = false)
+        {
+            if (parent == null) return;
+            if (parent.childCount == 0) return;
+
+            var _child = parent.GetChild(parent.childCount - 1).gameObject;
+            if (immediate)
+                Object.DestroyImmediate(_child);
+            else
+                Object.Destroy(_child);
+        }
+    }
+}

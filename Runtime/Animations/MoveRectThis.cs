7e005b2ce45d2b653e19ccbd1c471c490570c8ed
Agung Gunawan <agg.nst772@gmail.com>
2025-08-27 22:21:47 +0700

    Move Rect This

---
+++ Runtime/Animations/MoveRectThis.cs
@@ -0,0 +1,39 @@
+using System;
+using Gamecore.Tool;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Gamecore.Animation
+{
+    public class MoveRectThis : AnimationBase
+    {
+        [SerializeField] private bool isFrom;
+        [SerializeField, ShowIf(nameof(isFrom)), PickFromScene] private Vector3 from;
+        [SerializeField, PickFromScene] private Vector3 to;
+        
+        [SerializeField] private float time = 1;
+        [SerializeField] private LeanTweenType type;
+        
+        private RectTransform rectTransform;
+
+        private void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            if (isFrom && !base.autoPlay)
+            { 
+                rectTransform.anchoredPosition= from;
+            }
+        }
+
+        public override void Play()
+        {
+            base.Stop();
+            if (isFrom && base.autoPlay)
+            { 
+                rectTransform.anchoredPosition= from;
+            }
+            base.descr = rectTransform.LeanMove(to, time).setEase(type).setIgnoreTimeScale(base.useUnScaledTime);
+            base.Play();
+        }
+    }
+}

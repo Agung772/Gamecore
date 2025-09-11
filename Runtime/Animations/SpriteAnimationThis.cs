1c957a9f15a33105dd0adf998198721eeaf1f84f
Agung Gunawan <agg.nst772@gmail.com>
2025-09-12 01:17:04 +0700

    SpriteAnimationThis

---
+++ Runtime/Animations/SpriteAnimationThis.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Gamecore
+{
+    public class SpriteAnimationThis : MonoBehaviour
+    {
+        [SerializeField] protected bool autoPlay = true;
+        [SerializeField] private bool isLoop;
+        [SerializeField] private float fps = 12;
+        [SerializeField] private Sprite[] sprites;
+        [SerializeField, HideIf(nameof(isLoop))] private UnityEvent onComplete;
+        
+        private SpriteRenderer spriteRenderer;
+        private Image image;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            image = GetComponent<Image>();
+        }
+
+        private void OnEnable()
+        {
+            if (autoPlay)
+            {
+                Play();
+            }
+        }
+
+        private void OnDisable()
+        {
+            gameObject.StopCoroutine();
+        }
+
+        public void Play()
+        {
+            if (isLoop)
+            {
+                gameObject.StartCoroutineLoop(PlayCoroutine);
+            }
+            else
+            {
+                gameObject.StartCoroutine(PlayCoroutine);
+            }
+        }
+
+        public void Stop()
+        {
+            gameObject.StopCoroutine();
+        }
+
+        private IEnumerator PlayCoroutine()
+        {
+            foreach (var _sprite in sprites)
+            {
+                if (spriteRenderer) spriteRenderer.sprite = _sprite;
+                else if (image) image.sprite = _sprite;
+                yield return new WaitForSeconds(fps);
+            }
+
+            if (!isLoop)
+            {
+                onComplete?.Invoke();
+            }
+        }
+
+        public void Destroy()
+        {
+            Destroy(gameObject);
+        }
+    }
+}

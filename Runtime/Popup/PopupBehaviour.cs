0373b970b4cacbd182cf50199ab9ba3dc5fb63a2
Agung Gunawan <agg.nst772@gmail.com>
2025-09-13 00:54:49 +0700

    Rev Refactor Popup v3

--- Runtime/Popup/PopupBehaviour.cs
+++ Runtime/Popup/PopupBehaviour.cs
@@ -13,6 +13,7 @@ namespace Gamecore
         [FoldoutGroup("Base"), ShowIf(nameof(setOrder)), InfoBox("Default Order = 1")] public int sortOrder = 2;
 
         [FoldoutGroup("Base")] [SerializeField] private bool autoClose;
+        [FoldoutGroup("Base")] [SerializeField] private bool useUnScaledTime;
         [FoldoutGroup("Base")] [SerializeField, ShowIf("autoClose")] private int closeAfter;
         [FoldoutGroup("Base")] [SerializeField] private Button closeBtn;
         
@@ -27,7 +28,7 @@ namespace Gamecore
 
             if (autoClose)
             {
-                gameObject.LeanDelayedCall(closeAfter, Close);
+                gameObject.LeanDelayedCall(closeAfter, Close).setIgnoreTimeScale(useUnScaledTime);;
             }
         }
     
